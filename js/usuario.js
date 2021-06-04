1511258c3f30d6e0c888d584a0a9aa3e401eff0b
Beltraneds <beltranjr.alexander@gmail.com>
2021-06-03 23:17:11 -0400

    Confirmado

--- js/usuario.js
+++ js/usuario.js
@@ -1,26 +1,14 @@
-// guardaremos en el espacio dentro del navegadorsolo rut, nombre y fecha
-
-
 class usuario{
-    rut;
     nombre;
-    fecha;
-    //  mutadores
 
-    // que tal clase guarde como parametro r, n o fecha
-    setRut(rut){this.rut=rut;}
+    //  mutadores
     setNombre(nombre){this.nombre=nombre;}
-    setFecha(fecha){this.fecha=fecha;}
 
     // accesador
-    getRut(){return this.rut;}
     getNombre(){return this.nombre;}
-    getFecha(){return this.getFecha;}
 
     // toString()
-    // necesitamos crear el objeto
     imprimir(){
-        return 'Rut: '+this.rut+ ' Nombre: ' +this.nombre+ ' Fecha: '+this.fecha;
-
+        return 'Nombre: ' +this.nombre;
     }
 }
